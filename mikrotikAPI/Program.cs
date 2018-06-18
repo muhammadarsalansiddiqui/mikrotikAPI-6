f2b9e9845c51e60db44a9c3fce9ee0159e9e4524
potatoprince <martins.berzins16@gmail.com>
2018-06-18 11:38:15 -0700

    added  fail method (qol)

--- mikrotikAPI/Program.cs
+++ mikrotikAPI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net.Sockets;
 
 namespace mikrotikAPI
 {
@@ -6,7 +8,28 @@ namespace mikrotikAPI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MK mikrotik = new MK("10.155.108.15");
+            if (!mikrotik.Login("admin", ""))
+            {
+                loginFail();
+                mikrotik.Close();
+                return;
+            }
+            mikrotik.Send("/system/identity/getall");
+            mikrotik.Send(".tag=sss", true);
+            foreach (string h in mikrotik.Read())
+            {
+                Console.WriteLine(h);
+
+            }
+            Console.ReadKey();
+
+
+        }
+
+        private static void loginFail()
+        {
+            Console.WriteLine("Could not log in");
         }
     }
 }
